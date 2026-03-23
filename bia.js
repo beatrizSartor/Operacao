3f389056e7ca83e1454cbb056786a9b6c3acf645
I1HSA <sartorbeatriz155@gmail.com>
2026-03-23 11:15:57 -0300

    adicao

--- bia.js
+++ bia.js
@@ -1,1 +1,4 @@
-let numUm 
+let numUm = Number(prompt("Insira o primeiro numero:"));
+let numDois = Number(prompt("Insira o segundo numero:"));
+let adicao = Number(numUm+numDois);
+alert(`A adição de ${numUm} e ${numDois} é igual a ${adicao}`); 

5dd11f2bc8d4f9a071f732cdac20212437b232e9
I1HSA <sartorbeatriz155@gmail.com>
2026-03-30 08:47:38 -0300

    correção de histórico da branch

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ let numUm = Number(prompt("Digite o primeiro numero: "));
 let numDois = Number(prompt("Digite o segundo numero: "));
 let resultado = numUm - numDois;
 alert("O resultado da subtração é: " + resultado);
+
+let adicao = Number(numUm+numDois);
+alert(`A adição de ${numUm} e ${numDois} é igual a ${adicao}`); 

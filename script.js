4cf3707bcd19f56912e0fede4d183f0848a55e4d
I1HSA <emillynathaly42@gmail.com>
2026-03-30 08:16:30 -0300

    operacao de subtraçao

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let numUm = Number(prompt("Digite o primeiro numero: "));
 let numDois = Number(prompt("Digite o segundo numero: "));
+
 let resultado = numUm - numDois;
 alert("O resultado da subtração é: " + resultado);
+
+let divisao = numUm/numDois;
+alert(`O resultado da Divisão é ${divisao}`);
